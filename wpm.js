83d02371efa1a41cdc07b9956c46b20718a9fd39
Audrey Hasson <amhasson4@gmail.com>
2020-04-05 21:38:20 -0400

    timer!

--- wpm.js
+++ wpm.js
@@ -1,29 +1,29 @@
 // timer
 console.log("banana");
 
-var seconds = 60.00;
 
-function myTimer(time) {
-  time = time - .01;
-  return time;
-}
+var seconds = 60;
 document.getElementById("timer").innerHTML = seconds;
 
-function countdown() {
-  if (seconds>0) {
-    window.setInterval(myTimer(seconds), 10)
-  };
-  if (seconds === 0){
-
-  }
+document.getElementById("optimism").onclick = function myTimer() {
+    setInterval(function (){
+      if (seconds===-0.009999999996633669) {
+        document.getElementById("timer").innerHTML = "Done!";
+      }
+      else if (seconds>0) {
+      seconds = seconds - .01;
+      document.getElementById("timer").innerHTML = seconds.toFixed(2);
+    }
+  }, 10);
 };
-
-countdown();
-
-document.getElementById("optimism").onclick = countdown()
 
 
 document.getElementById("testbutt").onclick = function testFunction() {
   let head = document.getElementById("head");
   head.classList.toggle("myStyle");
 };
+
+function minusOne(num1) {
+  let num2 = num1 - 1;
+  return num2;
+}
